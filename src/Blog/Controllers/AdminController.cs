a256e32758e01332e256410608359deae5d70280
Enrage <enragesoft@gmail.com>
2016-08-06 21:05:56 +0300

    Added admin controller

---
+++ src/Blog/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Blog.Models.Account;
+using Blog.Models.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private UserManager<BlogUser> userManager { get; }
+        private IPostService postService { get; }
+
+        public AdminController(IPostService postService, UserManager<BlogUser> userManager)
+        {
+            this.userManager = userManager;
+            this.postService = postService;
+        }
+
+        public IActionResult Manage()
+        {
+            //get all unpublished posts
+            var posts = postService.GetAll()
+                .Where(post => post.IsPublished == false)
+                .ToList();
+
+            var viewModel = ModelFactory.Create(posts);
+
+            return View(viewModel);
+        }
+
+    }
+}
