3ba1dd350ceb4b97353ffa95a3a97603977f6d03
Dan <enragesoft@gmail.com>
2016-08-12 16:44:43 +0300

    Added new view model

---
+++ src/Blog/ViewModels/ControlPanelViewModels/PostControlPanelViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.ViewModels.ControlPanelViewModels
+{
+    public class PostControlPanelViewModel
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public DateTime PostedOn { get; set; }
+        public bool IsPublished { get; set; }
+    }
+}
