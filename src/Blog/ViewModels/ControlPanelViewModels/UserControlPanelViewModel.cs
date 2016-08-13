a8236e0919c726c1084ec6a46b389c6f0cf750fd
Dan <enragesoft@gmail.com>
2016-08-13 22:49:28 +0300

    Added new view model

---
+++ src/Blog/ViewModels/ControlPanelViewModels/UserControlPanelViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.Controllers
+{
+    public class UserControlPanelViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public bool isBanned { get; set; }
+    }
+}
