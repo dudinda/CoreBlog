485cda30df83c38ba6c1f93f8d797ad4337b5174
Dan <enragesoft@gmail.com>
2016-08-15 23:00:24 +0300

    Added registration controller, small refactoring

---
+++ src/Blog/wwwroot/js/Registration/registrationFactory.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('registrationFactory', ['$http', registrationFactory]);
+
+
+    function registrationFactory($http) {
+
+        function getRegistrationForm() {
+            return $http.get('/api/reg/form');
+        }
+
+        function validateNewUser(user) {
+            return $http.post('reg/registration', user);
+        }
+
+        var service = {
+            getRegistrationForm: getRegistrationForm,
+            validateNewUser: validateNewUser
+
+        };
+
+        return service;
+    }
+})();
