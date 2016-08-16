279a9e28c4e56c8f8a179ecf83522e518c66c300
Dan <enragesoft@gmail.com>
2016-08-16 20:19:11 +0300

    Changed CreatePost view using AngularJS

---
+++ src/Blog/wwwroot/js/CreatePost/postCreateController.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('blogApp')
+        .controller('postCreateController', ['$scope', 'postCreateFactory', postCreateController]);
+
+    function postCreateController($scope, postCreateFactory) {
+
+        var vm = this;
+        vm.error = "";
+
+        $scope.newPost = {};
+
+        vm.isFull = false;
+
+        postCreateFactory
+            .getPostCreateForm().success(function (response) {
+                angular.copy(response, $scope.newPost);
+            }).error(function () {
+                vm.error = "Oops. Something went wrong. Try again later!";
+            });
+
+
+        $scope.addTag = function () {
+            if ($scope.newPost.tags.length < 5) {
+                $scope.newPost.tags.push({
+                    name: ''
+                });
+            } else {
+                isFull = true;
+            }
+        };
+        
+        $scope.removeTag = function (index) {
+            vm.isFull = false;
+            $scope.newPost.tags.splice(index, 1);
+        };
+
+        vm.approve = function () {
+            postCreateFactory
+                .sendNewPost($scope.newPost).error(function () {
+                    vm.error = "Oops. Something went wrong. Try again later!";
+                });
+        };
+    }
+})();
---
+++ src/Blog/wwwroot/js/CreatePost/postCreateFactory.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('blogApp')
+        .factory('postCreateFactory', ['$http', postCreateFactory]);
+
+
+    function postCreateFactory($http) {
+
+        function getPostCreateForm() {
+            return $http.get('/api/post/create');
+        }
+
+        function sendNewPost(post) {
+            return $http.post('/api/post/submit', post);
+        }
+
+        var service = {
+            getPostCreateForm: getPostCreateForm,
+            sendNewPost: sendNewPost
+
+        };
+
+        return service;
+    }
+})();
