01bb5e07da18376128c14dd76aecf76dc9139a50
Dan <enragesoft@gmail.com>
2016-08-18 21:35:35 +0300

    Added postEditorController

---
+++ src/Blog/wwwroot/js/Admin/postEditorController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('adminApp')
+        .controller('postEditorController', ['$routeParams','controlPanelFactory', postEditorController]);
+
+    function postEditorController($routeParams, controlPanelFactory) {
+
+        var vm = this;
+        vm.post = {};
+        vm.error = "";
+        vm.id = $routeParams.id;
+
+        controlPanelFactory.getPost(vm.id).success(function (response) {
+            angular.copy(response, vm.post);
+        }).error(function () {
+            vm.error = "Failed to get the post with id: " + vm.id;
+        });
+        
+
+    };
+})();
