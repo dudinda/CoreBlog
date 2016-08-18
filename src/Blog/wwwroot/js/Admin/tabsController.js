bafcaf6a6b59eacfb021df1f05ff3d18fc749a15
Dan <enragesoft@gmail.com>
2016-08-18 14:53:11 +0300

    Added tabs controller

---
+++ src/Blog/wwwroot/js/Admin/tabsController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('adminApp')
+        .controller('tabsController', [tabsController]);
+
+    function tabsController() {
+        var vm = this;
+        vm.tabs = [
+            { link: '#/unpublished', label: 'New' },
+            { link: '#/published', label: 'Published' },
+            { link: '#/users', label: 'Users' }
+        ];
+
+        vm.selectedTab = vm.tabs[0];
+
+        vm.setSelectedTab = function (tab) {
+            vm.selectedTab = tab;
+        };
+
+        vm.tabClass = function (tab) {
+            return vm.selectedTab === tab ? "active" : "";
+        };
+    };
+})();
