39cf2a318b32456f1414a45357b2feb175b77e8f
Dan <enragesoft@gmail.com>
2016-08-18 14:52:53 +0300

    Added routing

---
+++ src/Blog/wwwroot/js/Admin/adminApp.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+    angular.module('adminApp', ['ngRoute'])
+       .config(function ($routeProvider) {
+           $routeProvider
+               .when("/published", {
+                   controller: "postController",
+                   controllerAs: "vm",
+                   templateUrl: "/views/managePublishedView.html"
+               })
+               .when("/unpublished", {
+                   controller: "postController",
+                   controllerAs: "vm",
+                   templateUrl: "/views/manageUnpublishedView.html"
+               })
+                .when("/users", {
+                    controller: "userController",
+                    controllerAs: "vm",
+                    templateUrl: "/views/manageUsersView.html"
+                });
+
+           $routeProvider.otherwise({ redirectTo: "/unpublished" });
+       });
+
+})();
---
+++ src/Blog/wwwroot/js/CreatePost/createPostApp.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('postCreateApp', ['ngRoute'])
+      .config(function ($routeProvider) {
+          $routeProvider.when("/", {
+              controller: "postCreateController",
+              controllerAs: "vm",
+              templateUrl: "/views/postCreateView.html"
+          });
+
+          $routeProvider.otherwise({ redirectTo: "/" });
+      });
+})();
