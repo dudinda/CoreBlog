1343626efec578f0d9cd6a21f2635b675335e173
Dan <enragesoft@gmail.com>
2016-08-19 21:50:48 +0300

    Added Image entity

---
+++ src/Blog/Models/Entities/Image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Entities
+{
+    public class Image
+    {
+        public int id { get; set; }
+        public int PostId { get; set; }
+        public int Filesize { get; set; }
+        public string Filetype { get; set; }
+        public string Filename { get; set; }
+        public string Base64 { get; set; }
+    }
+}
