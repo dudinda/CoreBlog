e5bbdddeebba7dd3822d8c27b83cb954940eb54b
Dan <enragesoft@gmail.com>
2016-08-24 12:40:55 +0300

    Added jquery redirect script

---
+++ src/Blog/wwwroot/js/JqueryScripts/redirect.js
@@ -0,0 +1,23 @@
+﻿function timer(seconds, url) {
+
+    $('#timer').text(seconds);
+
+    $('#timer').show();
+
+    var interval = setInterval(function () {
+
+        --seconds;
+
+        $('#timer').text(seconds);
+
+        if (seconds == 0) {
+
+            clearInterval(interval);
+            window.location = url;
+            $('timer').hide();
+
+        }
+
+    }, 1000);
+
+};
