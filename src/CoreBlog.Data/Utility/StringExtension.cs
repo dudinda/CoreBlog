d8166795771af054f2787779ace51298efb48fee
Dan <enragesoft@gmail.com>
2016-08-27 20:46:23 +0300

    Added string extension method

---
+++ src/CoreBlog.Data/Utility/StringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.Data.Utility
+{
+    public static class StringExtension
+    {
+        public static bool Contains(this string source, string toSearch, StringComparison cmp)
+        {
+            if(source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return ContainsImpl(source, toSearch, cmp);
+        }
+
+
+        private static bool ContainsImpl(this string source, string toSearch, StringComparison cmp)
+        {
+            return source.IndexOf(toSearch, cmp) >= 0;
+        }
+    }
+}
